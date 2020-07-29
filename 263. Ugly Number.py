1e3b0b66351b2f1078b2e08f2c2a4c02c3626b8b
Rajnish Singh <41592584+rajnish-13547@users.noreply.github.com>
2020-07-29 20:51:38 +0530

    Update 263. Ugly Number.py

--- 263. Ugly Number.py
+++ 263. Ugly Number.py
@@ -1,30 +1,29 @@
-class Solution:      
+class Solution:
+    def digitsum(self,num):
+        cnt = 0
+        while num !=0:
+            cnt+=num%10
+            num = num//10
         
-    def isUgly(self, num) -> bool:
-        def divide(N=1):
-            if N<=0:
-                return 0
-            elif N ==1: 
-                return 1
-            elif N ==2 or N ==3 or N ==5:
-                return 1
-        
-            A = str(N)
-            val = 0
-            for x in A:
-                val +=int(x)
-        
-            if A[-1]=='5' or A[-1]=='0':
-                return divide(int(N/5))
-            elif int(A[-1])%2==0:
-                return  divide(int(N/2))
-            elif val%3==0:
-                return divide(int(N/3))
-            else:
-                return N
-        
-        if divide(num)==1:
-            return True
+        return cnt
+    
+    def isUgly(self,num):
+        if num==1:
+            return 1
+        elif num<=0:
+            return 0
         else:
-            return False
-        
+            if num%10==0:
+                return self.isUgly(num//10)
+            elif num%10 ==5:
+                return self.isUgly(num//5)
+            elif self.digitsum(num)%9==0:
+                return self.isUgly(num//9)
+            elif self.digitsum(num)%3==0:
+                return self.isUgly(num//3)
+            elif num%4 ==0:
+                return self.isUgly(num//4)
+            elif num%2 ==0:
+                return self.isUgly(num//2)
+            else:
+                return num==1
