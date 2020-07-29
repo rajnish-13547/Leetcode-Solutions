731c608f5457c0698374c0c9d574fcd7c4945e9f
Rajnish Singh <41592584+rajnish-13547@users.noreply.github.com>
2020-07-29 21:38:30 +0530

    Create 294. Ugly Number 2.py

---
+++ 294. Ugly Number 2.py
@@ -0,0 +1,28 @@
+import math
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        i2 = 0
+        i3 = 0
+        i5 = 0
+        
+        out = [1]
+        while len(out)!= n:
+            
+            #Important
+            A2,A3,A5 = out[i2]*2,out[i3]*3,out[i5]*5
+            
+            A = min(A2,A3,A5)   
+            
+            if A2== A:
+                i2+=1
+
+            if A3== A:
+                i3+=1       
+            
+            if A5== A:
+                i5+=1
+                     
+            out.append(A)
+        
+        print(out)
+        return out[-1]
